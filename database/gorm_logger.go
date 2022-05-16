c307964cf4fdb1ca8fd4daac71e27a9f71bfc5f9
Karthik Raman <kartraman@gmail.com>
2022-05-16 13:34:05 +0530

    updated the databse logging

--- database/gorm_logger.go
+++ database/gorm_logger.go
@@ -29,7 +29,14 @@ func (gl *GormLogger) Warn(ctx context.Context, m string, v ...interface{}) {
 	gl.log.Info(m, v)
 }
 func (gl *GormLogger) Error(ctx context.Context, m string, v ...interface{}) {
-	gl.log.Error(ctx.Err(), m, v)
+	gl.log.Error(ctx.Err(), m, "values", v)
 }
 func (gl *GormLogger) Trace(ctx context.Context, begin time.Time, fc func() (sql string, rowsAffected int64), err error) {
+	elapsed := time.Since(begin)
+	sql, rows := fc()
+	if err != nil {
+		gl.log.Error(err, "trace", "sql", sql, "rows_affected", rows, "elapsed", time.Duration(elapsed))
+	} else {
+		gl.log.Info("trace", "sql", sql, "rows_affected", rows, "elapsed", time.Duration(elapsed))
+	}
 }
